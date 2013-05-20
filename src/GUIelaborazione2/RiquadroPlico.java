cc85d3dc883a3e8d8b0d82b8ce76930514c00fe6
Francesco Miscia <fra.miscia@gmail.com>
2013-05-20 13:05:01 +0200

    aaaa

--- src/GUIelaborazione2/RiquadroPlico.java
+++ src/GUIelaborazione2/RiquadroPlico.java
@@ -11,13 +11,29 @@ import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JToolBar;
 import javax.swing.UIManager;
+import java.awt.Dimension;
+import java.awt.Color;
+import javax.swing.JLayeredPane;
+import javax.swing.SwingConstants;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.border.LineBorder;
+import javax.swing.BoxLayout;
+import java.awt.CardLayout;
 
 @SuppressWarnings("serial")
 public class RiquadroPlico extends JPanel {
+
+	private JPanel plico;
+
 	public RiquadroPlico() {
 
 		setBackground(UIManager.getColor("Panel.background"));
 		setLayout(new BorderLayout());
+		
+				this.plico = new JPanel();
+				plico.setBackground(new Color(222, 184, 135));
+				add(plico, BorderLayout.CENTER);
 
 		JToolBar toolBar = new JToolBar();
 		add(toolBar, BorderLayout.NORTH);
@@ -28,21 +44,55 @@ public class RiquadroPlico extends JPanel {
 		toolBar.setAlignmentY(Component.CENTER_ALIGNMENT);
 		toolBar.setFloatable(false);
 
-		JButton btnNewButton = new JButton("Crea Distinta");
-		toolBar.add(btnNewButton);
+		JButton creaDistButton = new JButton("Crea Distinta");
+		toolBar.add(creaDistButton);
 
-		JButton btnNewButton_1 = new JButton("Elimina Commessa");
-		toolBar.add(btnNewButton_1);
+		JButton deleteCommButton = new JButton("Elimina Commessa");
+		toolBar.add(deleteCommButton);
 
 		JButton btnNewButton_2 = new JButton("New button");
 		toolBar.add(btnNewButton_2);
 
 		JButton btnNewButton_3 = new JButton("New button");
 		toolBar.add(btnNewButton_3);
-		
-		JPanel plico = new JPanel();
-		add(plico, BorderLayout.CENTER);
 
+		JPanel clipPanel = new JPanel();
+		clipPanel.setBorder(new LineBorder(new Color(160, 82, 45), 2));
+		clipPanel.setBackground(new Color(222, 184, 135));
+		clipPanel.setPreferredSize(new Dimension(150, 100));
+		add(clipPanel, BorderLayout.EAST);
+		clipPanel.setLayout(null);
+
+		JButton commessaButton = new JButton("Commessa");
+		commessaButton.setToolTipText("Commessa");
+		commessaButton.setBounds(20, 5, 110, 25);
+		commessaButton.setHorizontalTextPosition(SwingConstants.LEFT);
+		commessaButton.setPreferredSize(new Dimension(90, 25));
+		clipPanel.add(commessaButton);
+		commessaButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				PlicoCommessa plico_commessa = new PlicoCommessa();
+				plico_commessa.load();
+				plico.removeAll();
+				plico.add(plico_commessa);
+			}
+		});
+
+		JButton addButton = new JButton("+");
+		addButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				PlicoDistinta plico_distinta = new PlicoDistinta();
+				plico_distinta.load();
+				plico.removeAll();
+				plico.add(plico_distinta);
+			}
+		});
+		addButton.setToolTipText("Aggiungi ");
+		addButton.setBounds(20, 35, 110, 25);
+		clipPanel.add(addButton);
+		addButton.setPreferredSize(new Dimension(110, 25));
+		addButton.setMinimumSize(new Dimension(110, 25));
+		addButton.setMaximumSize(new Dimension(110, 25));
 
 	}
 }
