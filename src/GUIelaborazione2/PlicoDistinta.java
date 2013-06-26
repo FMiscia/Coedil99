357aef0cdd0a55c0644e8edc783267fe4fd60d16
cicerone <federico.cicerone@gmail.com>
2013-06-26 13:07:24 +0200

    marck

--- src/GUIelaborazione2/PlicoDistinta.java
+++ src/GUIelaborazione2/PlicoDistinta.java
@@ -1,13 +1,34 @@
 package GUIelaborazione2;
 
+import java.awt.Dimension;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
+
+import GUIelaborazione2.Riquadri.RiquadroDatiDistinta;
+
 public class PlicoDistinta extends Plico{
 
 	
 	private static PlicoDistinta instance = null;
-	private RigaDistinta rd = new RigaDistinta();
+	private RiquadroDatiDistinta rd;
 	
-	public PlicoDistinta(){
+	private PlicoDistinta(){
+		setBorder(null);
+		setLayout(null);
+		
+		rd = new RiquadroDatiDistinta("Lavoro");
+		rd.setBounds(40, 20,rd.getWidth(),rd.getHeight());
+		setPreferredSize(new Dimension(745,1000));
+		setSize(745,950);
 		this.add(rd);
+		
+		rd.addComponentListener(new ComponentAdapter() {
+			@Override
+			public void componentResized(ComponentEvent e) {
+				validate();
+				repaint();
+			}
+		});
 	}
 
 	@Override
@@ -16,16 +37,18 @@ public class PlicoDistinta extends Plico{
 		
 	}
 
-	@Override
-	public void load(Object o) {
-		// TODO Auto-generated method stub
-		
-	}
+
 	
 	public static PlicoDistinta getInstance(){
 		if(PlicoDistinta.instance == null)
 			PlicoDistinta.instance = new PlicoDistinta();
 		return PlicoDistinta.instance;
+	}
+
+	@Override
+	public void load(int id) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
