03b0855bbe91f40277ad207e7d92b268bcf2d86c
Francesco Miscia <fra.miscia@gmail.com>
2013-07-09 18:41:50 +0200

    Load Distinta

--- src/GUIelaborazione2/PlicoDistinta.java
+++ src/GUIelaborazione2/PlicoDistinta.java
@@ -1,45 +1,48 @@
 package GUIelaborazione2;
 
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
+import java.util.Iterator;
+
+import elaboradistinta.controller.GestisciCommessaHandler;
+import elaboradistinta.model.Distinta;
+import elaboradistinta.model.RigaLavoro;
 
 import GUIelaborazione2.Riquadri.RiquadroDatiDistinta;
 
-public class PlicoDistinta extends Plico{
+public class PlicoDistinta extends Plico {
 
-	
 	private static PlicoDistinta instance = null;
-	private RiquadroDatiDistinta rd;
-	
-	private PlicoDistinta(){
+
+	private PlicoDistinta() {
 		setBorder(null);
-		setLayout(null);
-		
-		rd = new RiquadroDatiDistinta("Lavoro");
-		rd.setBounds(40, 20,rd.getWidth(),rd.getHeight());
-		setPreferredSize(new Dimension(745,1000));
-		setSize(745,950);
-		this.add(rd);
-		
-		rd.addComponentListener(new ComponentAdapter() {
-			@Override
-			public void componentResized(ComponentEvent e) {
-				validate();
-				repaint();
-			}
-		});
+		setLayout(new FlowLayout());
+
+		// rd = new RiquadroDatiDistinta("Lavoro");
+		// rd.setBounds(40, 20,rd.getWidth(),rd.getHeight());
+		setPreferredSize(new Dimension(745, 1000));
+		setSize(745, 950);
+		// this.add(rd);
+
+		// rd.addComponentListener(new ComponentAdapter() {
+		// @Override
+		// public void componentResized(ComponentEvent e) {
+		// validate();
+		// repaint();
+		// }
+		// });
 	}
 
 	@Override
 	public void load() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
-
-	public static PlicoDistinta getInstance(){
-		if(PlicoDistinta.instance == null)
+	public static PlicoDistinta getInstance() {
+		if (PlicoDistinta.instance == null)
 			PlicoDistinta.instance = new PlicoDistinta();
 		return PlicoDistinta.instance;
 	}
@@ -47,7 +50,23 @@ public class PlicoDistinta extends Plico{
 	@Override
 	public void load(int id) {
 		// TODO Auto-generated method stub
-		
+		this.removeAll();
+		Distinta d = (Distinta) GestisciCommessaHandler.getInstance()
+				.getCommessaById(id).getDistinta();
+		if (d != null && d.getLavori().size()!= 0) {
+			for(int i=0; i<d.getLavori().size(); i++){
+				RiquadroDatiDistinta temp = new RiquadroDatiDistinta(
+						"Riga Lavoro");
+				temp.load(d.getLavori().get(i));
+				this.add(temp);
+			}
+		}
+		else{
+			this.add(new RiquadroDatiDistinta(
+					"Riga Lavoro"));
+		}
+		this.validate();
+		this.repaint();
 	}
 
 }
