66e23774ea7a3f130622622e55bef1272ecbe966
cicerone <federico.cicerone@gmail.com>
2013-10-06 16:13:06 +0200

    in attesa del riquadro

--- src/GUI/RDACenter.java
+++ src/GUI/RDACenter.java
@@ -8,9 +8,9 @@ import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 
+import GUI.Abstract.RiquadroCodici;
 import GUI.ClipPanels.ClipPanelRDA;
 import GUI.Plichi.PlicoRDA;
-import GUI.Riquadri.RiquadroCodici;
 import GUI.Riquadri.RiquadroCodiciRDA;
 
 public class RDACenter extends JPanel {
--- src/GUI/Riquadri/RiquadroCodiciRDA.java
+++ src/GUI/Riquadri/RiquadroCodiciRDA.java
@@ -6,23 +6,35 @@ import java.awt.Component;
 import java.awt.Dimension;
 import java.util.ArrayList;
 
-import coedil99.controller.GestisciClienteHandler;
 import coedil99.controller.GestisciCommessaHandler;
+import coedil99.controller.GestisciRDAHandler;
 import coedil99.model.Cliente;
+import coedil99.model.RDA;
+
+import GUI.Abstract.ARiquadroCodici;
 
 @SuppressWarnings("serial")
-public class RiquadroCodiciRDA extends RiquadroCodici {
+public class RiquadroCodiciRDA extends ARiquadroCodici {
 	
 	
 	public RiquadroCodiciRDA(){
 		super();
+		this.load(new ArrayList<Object>(GestisciRDAHandler.getInstance().getArrayRDA()));
 	
 	}
 	
 	@Override
 	public void load(ArrayList<Object> t){
 
-		
+		int row = GestisciRDAHandler.getInstance().getNumOfRDA();
+		this.panel.setPreferredSize(new Dimension(150,row*70));
+		for(int k=0; k<t.size(); ++k){
+			final RiquadroRDA r = new RiquadroRDA(this);
+			r.load(GestisciRDAHandler.getInstance().getArrayRDA().get(k));
+			panel.add(r);
+		}
+		this.setPreferredSize(new Dimension(154,panel.getHeight()));
+		this.getVerticalScrollBar().setPreferredSize (new Dimension(0,0));
 	}
 
 	public void deselectAll() {
