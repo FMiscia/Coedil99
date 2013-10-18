60a7a5da3f28eb95a8f92fa70b441d3f52965300
Simone <simondima89@gmail.com>
2013-10-18 20:10:35 +0200

    Aggiunto un nuovo tipo di scrollbar

--- src/GUI/Main.java
+++ src/GUI/Main.java
@@ -4,6 +4,9 @@ import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.UIManager;
+
+import com.alee.laf.scroll.WebScrollBarUI;
 
 public class Main {
 
@@ -36,6 +39,7 @@ public class Main {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
+		UIManager.put("ScrollBarUI", WebScrollBarUI.class.getName());
 		frame = new JFrame();
 		frame.setBounds(100, 100, 1000, 500);
 		frame.getContentPane().add(ProgrammaLavori.getInstance(),BorderLayout.CENTER);
