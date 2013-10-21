4673f8ced49d80781122e1162424dded775f46e0
Francesco Miscia <fra.miscia@gmail.com>
2013-10-21 17:59:45 +0200

    commenti

--- src/coedil99/model/Observer.java
+++ src/coedil99/model/Observer.java
@@ -4,6 +4,15 @@ import java.util.ArrayList;
 
 import coedil99.controller.GestisciRDAHandler;
 
+
+/**
+ * 
+ * @author francesco
+ * 
+ * Pattern Observer utilizzato per aggionrare lo stato delle merci a seconda dello stato
+ * delle RDA e perfornire notifiche all'utente sullo stato delle RDA
+ *
+ */
 public abstract class Observer {
 	
 	protected ArrayList<Subject> subjects = new ArrayList<Subject>();
