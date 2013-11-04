a06952bcafb577d58241305bf9108a5c7bbf3be3
cicerone <federico.cicerone@gmail.com>
2013-11-04 16:32:28 +0100

    login

---
+++ src/GUI/Login/Login.java
@@ -0,0 +1,24 @@
+package GUI.Login;
+
+public class Login {
+	
+	private static boolean logged = false;
+	
+    public static boolean authenticate(String username, String password) {
+        // hardcoded username and password
+        if (username.equals("admin") && password.equals("admin")) {
+        	logged = true;
+            return true;
+        }
+        return false;
+    }
+    
+    public static boolean getLogged() {
+    	return logged;
+    }
+
+	public static void logOut() {
+		// TODO Auto-generated method stub
+		logged = false;
+	}
+}
